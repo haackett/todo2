b08ae602f834e8d01ace791b7e0adf5126f9b2ca
Aidan Hackett <aodhanelectronic@gmail.com>
2020-11-29 14:23:51 -0500

    Revert "Merge pull request #1 from haackett/feat-reccuring"
    This reverts commit 2d7e71a12ed5a16582be648321ea68850ce55b20, reversing
    changes made to 76dc586b0eb6702ea69332749e003090f88731c3.
    
    Reverting to pre-merge branch. The merge broke production.#

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 from database import db
-import datetime
 
 class Todo(db.Model):
     __tablename__ = 'todo'
@@ -7,8 +6,6 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, unique=False, nullable=False)
     completed = db.Column(db.Boolean, unique=False, nullable=False, default=False)
-    initialDate = db.Column(db.DateTime, default=datetime.date.today())
-    recurrenceInterval = db.Column(db.Integer)
 
     @property
     def serialize(self):
